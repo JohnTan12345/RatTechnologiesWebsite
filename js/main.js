8e91b9d8a6d96e4d450141fc2e5cfab9e3c6ce41
John Tan <johntanhongyan@gmail.com>
2025-01-24 03:39:30 +0800

    modified body element, finished customize page layout

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
-function customize(item) {
+function customize(item, previousPage) {
     localStorage.setItem("item", item)
+    localStorage.setItem("previous", previousPage)
     window.location.href = "customize.html"
+}
+
+function customizepageloaded() {
+}
+
+function addtocart() {
+    alert(localStorage.getItem("previous"))
 }
