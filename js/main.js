65b22b332a300eea370273ea3f85d87f446ed817
John Tan <s10259175@connect.np.edu.sg>
2025-01-27 15:17:27 +0800

    Added feedback to reviews

--- js/main.js
+++ js/main.js
@@ -10,3 +10,23 @@ function customizepageloaded() {
 function addtocart() {
     alert(localStorage.getItem("previous"))
 }
+
+function helpfulagree(review_id) {
+    helpfulchange(review_id)
+}
+
+function helpfuldisagree(review_id) {
+    helpfulchange(review_id)
+}
+
+function helpfulchange(review_id) {
+    const reviewelements = document.getElementsByClassName(review_id)
+    for (var i = 0; i < reviewelements.length; i++) {
+        var element = reviewelements.item(i)
+        if (element.classList.contains("helpful-button")) {
+            element.style.display = "none"
+        } else if (element.classList.contains("helpful-text")) {
+            element.innerHTML = "Thank You for your feedback"
+        }
+    }
+}
